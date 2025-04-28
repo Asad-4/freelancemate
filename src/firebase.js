9ac527459b5e9a7c56c317362ae34b1309d1596b
Asadbek <asadbek@i2max.co.kr>
2025-04-28 14:22:04 +0900

    First commit: Initial Setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfluFciyaKQvfwG4l3llXT6KOZcvnXr_8",
+    authDomain: "freelancemate-2d7be.firebaseapp.com",
+    projectId: "freelancemate-2d7be",
+    storageBucket: "freelancemate-2d7be.firebasestorage.app",
+    messagingSenderId: "969770039020",
+    appId: "1:969770039020:web:699c1234d7617a5917d7c9"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
