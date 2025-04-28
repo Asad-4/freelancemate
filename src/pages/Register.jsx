3e3326aa2ba01b19af2ce4c723e342882e5b0883
Asadbek <asadbek@i2max.co.kr>
2025-04-28 14:50:08 +0900

    Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate('/dashboard');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleRegister}>
+        <h2>Register</h2>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
